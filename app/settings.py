7f0ca99c6a291e69cae69a923bc279607089d85e
David <fernando.gam@outlook.com>
2024-11-29 09:31:40 -0500

    added CORS config

--- app/settings.py
+++ app/settings.py
@@ -142,3 +142,14 @@ CACHES = {
         "LOCATION": os.environ.get("PATH_CACHE","/var/tmp/django_cache"),
     }
 }
+
+
+# CORS CONFIG 
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
